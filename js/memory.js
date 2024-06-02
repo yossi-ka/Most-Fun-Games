78a96b1cc387a7eb96a73e21853f72a43f75979e
yossi-ka <yk111067@gmail.com>
2024-06-02 13:30:36 +0300

    message

---
+++ js/memory.js
@@ -0,0 +1,35 @@
+//  setting the animals
+const snakeImg = "../assents/images/snake.png";
+const bearImg = "../assents/images/bear.jpg";
+const lionImg = "../assents/images/lion.jpg";
+const roosterImg = "../assents/images/rooster.jpg";
+const camelImg = "../assents/images/camel.jpg";
+const wolfImg = "../assents/images/wolf.webp";
+const cowImg = "../assents/images/cow.webp";
+const dolphinImg = "../assents/images/dolphin.jpg";
+const animalImages = [
+  snakeImg,
+  snakeImg,
+  bearImg,
+  bearImg,
+  lionImg,
+  lionImg,
+  roosterImg,
+  roosterImg,
+  camelImg,
+  camelImg,
+  dolphinImg,
+  dolphinImg,
+  wolfImg,
+  wolfImg,
+  cowImg,
+  cowImg,
+];
+const game = document.querySelector(".game")
+const cards = game.querySelectorAll(".card")
+const images = game.querySelectorAll("img")
+function {
+    let x = Math.floor(Math.random() * 16);
+while(images[x].getAttribute("img"))
+}
+
