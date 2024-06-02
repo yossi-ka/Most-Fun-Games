184ee863431ca2f22d8ab203968d764d19ba09dc
yossi-ka <yk111067@gmail.com>
2024-06-02 15:11:45 +0300

    message

--- js/memory.js
+++ js/memory.js
@@ -41,12 +41,42 @@ for (let i = 0; i < 16; i++) {
   images[getNum()].setAttribute("src", animalImages[i]);
 }
 //  game
-let left = 10;
-document.querySelector("h1").textContent = left;
-game.addEventListener("click", swich);
-function swich(event) {
+let counter = 0; //  how many cards are discover
+let left = 10; //  how many times are left to play
+const leftX = document.querySelector("h1");
+leftX.textContent = left;
+game.addEventListener("click", discover);
+function discover(event) {
   let currentP = event.target;
   let currentIndex = Array.from(p).indexOf(currentP);
+  if (
+    images[currentIndex].style.display === "block" ||
+    currentP.textContent === ""
+  )
+    return;
   currentP.style.display = "none";
   images[currentIndex].style.display = "block";
+  counter++;
+  if (counter === 2) {
+    game.removeEventListener("click", discover);
+  }
+}
+document.querySelector("button").addEventListener("click", cover);
+function cover() {
+  counter = 0;
+  game.addEventListener("click", discover);
+  for (let i = 0; i < 16; i++) {
+    if (p[i].textContent === "click" && p[i].style.display === "none") {
+      p[i].style.display = "block";
+      images[i].style.display = "none";
+    }
+  }
+  leftX.textContent = --left;
+  if (left == 0) youFailed();
+}
+function youFailed() {
+  document.querySelector(".youFailed").style.display = "block";
+}
+function youWin() {
+  document.querySelector(".youWin").style.display = "block";
 }
