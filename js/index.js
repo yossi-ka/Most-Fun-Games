c17420fa418a2a91555ad0ddd9c7efc821076ee3
yossi-ka <yk111067@gmail.com>
2024-06-03 17:00:14 +0300

    message

--- js/index.js
+++ js/index.js
@@ -12,20 +12,40 @@ signUpLink.addEventListener("click", () => {
   sign.style.display = "block";
   log.style.display = "none";
 });
-//  saves new customer
-forms[1].addEventListener("submit", () => {
-  const inputs = forms[1].querySelectorAll("input");
+//  check if the new customers' email is available
+const h5Ava = document.querySelector("h5");
+const mail = document.querySelector("#sign-email");
+let available = true;
+mail.addEventListener("input", () => {
+  let mailInput = mail.value;
   const usersArr = JSON.parse(localStorage.getItem("users-fun"));
-  const newCustomer = {
-    name: inputs[0].value,
-    email: inputs[1].value,
-    password: inputs[2].value,
-  };
-  usersArr.push(newCustomer);
-  localStorage.setItem("users-fun", JSON.stringify(usersArr));
-  sign.style.display = "none";
-  log.style.display = "block";
+  for (let i = 0; i < usersArr.length; i++) {
+    if (usersArr[i].email === mailInput) available = false;
+    else available = true;
+  }
+  if (!available) {
+    h5Ava.style.display = "block";
+  } else {
+    h5Ava.style.display = "none";
+  }
 });
+//  sign up the new customer
+if (available) {
+  forms[1].addEventListener("submit", () => {
+    const inputs = forms[1].querySelectorAll("input");
+    const usersArr = JSON.parse(localStorage.getItem("users-fun"));
+    const newCustomer = {
+      name: inputs[0].value,
+      email: inputs[1].value,
+      password: inputs[2].value,
+    };
+    usersArr.push(newCustomer);
+    localStorage.setItem("users-fun", JSON.stringify(usersArr));
+    sign.style.display = "none";
+    log.style.display = "block";
+  });
+}
+
 //  login for existing customers
 forms[0].addEventListener("submit", (event) => {
   event.preventDefault();
